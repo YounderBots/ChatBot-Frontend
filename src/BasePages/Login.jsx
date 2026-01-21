8a0af975ed56e80fd6eaffcff882320feebdde3b
Tharanya S.J <ttharanya19@gmail.com>
2026-01-21 15:15:17 +0530

    updated login page

--- src/BasePages/Login.jsx
+++ src/BasePages/Login.jsx
@@ -36,113 +36,120 @@ const Login = () => {
           />
         </div>
         {error && <div className='alert alert-danger py-2'>{error}</div>}
-        {!showReset ? (
-          <form onSubmit={handleSubmit}>
-            <div className='mb-3'>
-              <label className='form-label'>Email Address</label>
-              <input
-                type='email'
-                className='form-control'
-                required
-                autoComplete='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="mb-3 position-relative">
-              <label className="form-label">Password</label>
-              <input
-                type={showPassword ? "text" : "password"}
-                className="form-control"
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <span
-                className="password-toggle"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <Eye /> : <EyeOff />}
-              </span>
-            </div>
-
-
-            <div className='d-flex justify-content-between align-items-center mb-3'>
-              <div className='form-check'>
-                <input
-                  type='checkbox'
-                  className='form-check-input'
-                  id='rememberMe'
-                  checked={rememberMe}
-                  onChange={(e) => setRememberMe(e.target.checked)}
-                />
-                <label className='form-check-label' htmlFor='rememberMe'>Keep me Signed In</label>
-
-              </div>
-              <span className='forgot-link cursorPointer' onClick={() => setShowReset(true)}>Forgot Password?</span>
-
-            </div>
-
-            <button
-              type="submit"
-              className="btn btn-primary w-100"
-              disabled={loading}
-            >
-              {loading && (
-                <span className="spinner-border spinner-border-sm me-2"></span>
-              )}
-              {loading ? "Signing In..." : "Sign In"}
-            </button>
-
-          </form>
-
-        ) : (
-          <div>
-      <h5 className='mb-3 text-center'>Reset Password</h5>
-
-      {!resetSent ? (
-        <>
+        <form onSubmit={handleSubmit}>
           <div className='mb-3'>
             <label className='form-label'>Email Address</label>
             <input
               type='email'
               className='form-control'
-              placeholder='Enter your registered email'
-              value={resetEmail}
-              onChange={(e) => setResetEmail(e.target.value)}
+              required
+              autoComplete='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
+          <div className="mb-3 position-relative">
+            <label className="form-label">Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              className="form-control"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <span
+              className="password-toggle"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <Eye /> : <EyeOff />}
+            </span>
+          </div>
 
-          <button
-            className='btn btn-primary w-100'
-            onClick={() => setResetSent(true)}
-            disabled={!resetEmail}
-          >
-            Send Reset Link
-          </button>
-        </>
-      ) : (
-        <>
-          <div className='alert alert-success'>
-            Password reset link has been sent to your Email
+
+          <div className='d-flex justify-content-between align-items-center mb-3'>
+            {/* <div className='form-check'>
+              <input
+                type='checkbox'
+                className='form-check-input'
+                id='rememberMe'
+                checked={rememberMe}
+                onChange={(e) => setRememberMe(e.target.checked)}
+              />
+              <label className='form-check-label' htmlFor='rememberMe'>Keep me Signed In</label>
+
+            </div> */}
+            {/* <span className='forgot-link cursorPointer' onClick={() => setShowReset(true)}>Forgot Password?</span> */}
+
           </div>
 
           <button
-            className='btn btn-success w-100'
-            onClick={() => {
-              setShowReset(false);
-              setResetSent(false);
-            }}
+            type="submit"
+            className="btn btn-primary w-100"
+            disabled={loading}
           >
-            Back to Login
+            {loading && (
+              <span className="spinner-border spinner-border-sm me-2"></span>
+            )}
+            {loading ? "Signing In..." : "Sign In"}
           </button>
-        </>
-      )}
-    </div>
-        )}
+
+        </form>
 
       </Card>
+      {/* {showReset && (
+        <div className='modal fade show d-block' tabIndex="-1">
+          <div className='modal-dialog modal-dialog-centered'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>Reset Password</h5>
+                <button
+                  type='button'
+                  className='btn-close'
+                  onClick={() => {
+                    setShowReset(false);
+                    setResetSent(false);
+                  }}
+                />
+              </div>
+              <div className='modal-body'>
+                {!resetSent ?
+                  (<>
+                    <label className='form-label'> Email Address</label>
+                    <input
+                      type='email'
+                      className='form-control'
+                      placeholder='Enter your Registered Email'
+                      value={resetEmail}
+                      onChange={(e) => setResetEmail(e.target.value)} />
 
+
+                  </>
+                  ) : (
+                    <div className='alert alert-success'>Password reset link has been sent to your Email</div>
+                  )}
+
+              </div>
+              <div className='modal-footer'>
+                {!resetSent ? (
+                  <button
+                    className='btn btn-primary w-100'
+                    onClick={() => setResetSent(true)}
+                    disabled={!resetEmail}
+                  >Send Reset Link
+                  </button>
+                ) : (
+                  <button
+                    className='btn btn-secondary w-100'
+                    onClick={() => setShowReset(false)}>
+                    Close
+                  </button>
+                )}
+              </div>
+
+            </div>
+          </div>
+        </div>
+      )} */}
     </div>
   )
 }
